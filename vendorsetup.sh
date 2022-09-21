b34023166cb2ed21bdde9bfbf6e04cf6f26ac046
chickendrop89 <109487055+chickendrop89@users.noreply.github.com>
2022-09-21 16:45:20 +0200

    spes: vendorsetup: fix formatting, add FOX_ADVANCED_SECURITY

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -34,81 +34,84 @@ fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
-    # Fox-specific flags
-	export FOX_ENABLE_APP_MANAGER=0
-	export FOX_DELETE_AROMAFM=1
-	export FOX_DELETE_INITD_ADDON=1 # !- Causes bootloops sometimes -!
-	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"; # Sun 21 Mar 04:26:40 GMT 2021
-    export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+ # Fox-specific flags
+ export FOX_ENABLE_APP_MANAGER=0
+ export FOX_DELETE_AROMAFM=1
+ export FOX_DELETE_INITD_ADDON=1 # !- Causes bootloops sometimes -!
+ export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"; # Sun 21 Mar 04:26:40 GMT 2021
+ export OF_QUICK_BACKUP_LIST="/boot;/data;"
+ export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+ export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+ export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 
-	# Binaries & Tools
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_NANO_EDITOR=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export OF_ENABLE_LPTOOLS=1
+ # Binaries & Tools
+ export FOX_USE_BASH_SHELL=1
+ export FOX_ASH_IS_BASH=1
+ export FOX_USE_NANO_EDITOR=1
+ export FOX_USE_TAR_BINARY=1
+ export FOX_USE_SED_BINARY=1
+ export FOX_USE_XZ_UTILS=1
+ export OF_ENABLE_LPTOOLS=1
 
-	# Version & Variant
-	export FOX_VERSION="R11.1"
-    export FOX_VARIANT="A12.1"
+ # Version & Variant
+ export FOX_VERSION="R11.1"
+ export FOX_VARIANT="A12.1"
 
-    # MIUI & Custom ROMs
-	export OF_VIRTUAL_AB_DEVICE=1
-	export OF_NO_MIUI_PATCH_WARNING=1
-	export OF_PATCH_AVB20=1
+ # MIUI & Custom ROMs
+ export OF_VIRTUAL_AB_DEVICE=1
+ export OF_NO_MIUI_PATCH_WARNING=1
+ export OF_PATCH_AVB20=1
 
-	# Disable Flashlight & Green LED
-	export OF_FLASHLIGHT_ENABLE=0
-    export OF_USE_GREEN_LED=0
+ # Disable Flashlight & Green LED
+ export OF_FLASHLIGHT_ENABLE=0
+ export OF_USE_GREEN_LED=0
 
-	# Use magisk 25.2 for the magisk addon
-	export FOX_USE_SPECIFIC_MAGISK_ZIP="$PWD/device/xiaomi/spes/addon/Magisk-v25.2.zip"
-   
-	# Screen settings
-	export OF_SCREEN_H=2400
-	export OF_STATUS_H=-90
-	export OF_STATUS_INDENT_LEFT=48
-	export OF_STATUS_INDENT_RIGHT=48
-  	export OF_HIDE_NOTCH=1
-	export OF_ALLOW_DISABLE_NAVBAR=0
-	export OF_CLOCK_POS=1 # Left & Right
+ # Use magisk 25.2 for the magisk addon
+ export FOX_USE_SPECIFIC_MAGISK_ZIP="$PWD/device/xiaomi/spes/addon/Magisk-v25.2.zip"
 
-    # Maximum permissible splash image size (in kilobytes); do *NOT* increase!
-	export OF_SPLASH_MAX_SIZE=130
+ # Security (Disables MTP&ADB during password prompt)
+ export FOX_ADVANCED_SECURITY=1
 
-    # CCACHE
-    export USE_CCACHE=1
-    export CCACHE_EXEC=/usr/bin/ccache
+ # Screen settings
+ export OF_SCREEN_H=2400
+ export OF_STATUS_H=-70
+ export OF_STATUS_INDENT_LEFT=48
+ export OF_STATUS_INDENT_RIGHT=48
+ export OF_ALLOW_DISABLE_NAVBAR=0
+ export OF_HIDE_NOTCH=1
+ export OF_CLOCK_POS=1 # Left & Right
 
-    CCACHE_DIR="/media/${USERNAME}/ccache" 
+ # Maximum permissible splash image size (in kilobytes); do *NOT* increase!
+ export OF_SPLASH_MAX_SIZE=130
 
-    if [ -d ${CCACHE_DIR} ];
-    then
-      export CCACHE_DIR=${CCACHE_DIR}
-    else
-      echo "CCACHE Directory/Partition is not mounted at \"${CCACHE_DIR}\""
-      echo "Please edit the CCACHE_DIR build variable or mount the directory."
-    fi
+ # CCACHE
+ export USE_CCACHE=1
+ export CCACHE_EXEC=/usr/bin/ccache
 
-    # Debugging
-	## export FOX_RESET_SETTINGS=0
-	## export FOX_INSTALLER_DEBUG_MODE=1
+ CCACHE_DIR="/media/${USERNAME}/ccache"
 
-    # Other..
-	export TW_DEFAULT_LANGUAGE="en"
-	export LC_ALL="C"
+ if [ -d ${CCACHE_DIR} ];
+  then
+   export CCACHE_DIR=${CCACHE_DIR}
+  else
+   echo "CCACHE Directory/Partition is not mounted at \"${CCACHE_DIR}\""
+   echo "Please edit the CCACHE_DIR build variable or mount the directory."
+ fi
 
-	# Let's see what are our build VARs
-	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
- 	fi
+ # Debugging
+ ## export FOX_RESET_SETTINGS=0
+ ## export FOX_INSTALLER_DEBUG_MODE=1
+
+ # Other..
+ export TW_DEFAULT_LANGUAGE="en"
+ export LC_ALL="C"
+
+ # Let's see what are our build VARs
+ if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+  export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ fi
 fi
 #
